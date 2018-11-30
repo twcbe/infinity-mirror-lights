ae9d76ac919849f2d2b2342e2044275b767ef5ce
Sachin <sachinp@thoughtworks.com>
2018-11-30 17:22:48 +0530

    [Ashok/Sachin] Added admin UI and some fixes

--- app.js
+++ app.js
@@ -2,28 +2,28 @@ const express = require('express');
 const app =  express();
 const mqtt = require('mqtt');
 
-var mqttUrl = process.env.MQTT_URL || 'mqtt://user:pass@localhost:1883';
+var mqttUrl = process.env.MQTT_URL || 'mqtt://localhost:1883';
 var topic = process.env.MQTT_TOPIC || 'led_ring_server';
 
 var options = {
     clientId: 'mqttjs_' + Math.random().toString(16).substr(2, 8),
-    keepalive: 60,
-    reconnectPeriod: 1000,
-    protocolId: 'MQIsdp',
-    protocolVersion: 3,
-    clean: true,
-    encoding: 'utf8'
+    // protocolId: 'MQIsdp',
+    // protocolVersion: 3,
+    // clean: true,
+    // encoding: 'utf8'
 };
 
 var client = mqtt.connect(mqttUrl, options);
 
 var userColorMap = {}; // {userId: [userId, userChosenColor, timestamp]}
-var repeatFlag = false;
+var repeatFlag = true;
 var cycleSpeed = 120;
 var needsPublish = false;
+var startTimestamp = 0;
 
 function publishColors() {
     var colors = Object.values(userColorMap)
+        .filter(userData => userData.timestamp > startTimestamp || userData.userId.indexOf("admin") >= 0 )
         .map(userData => [userData.userChosenColor.r, userData.userChosenColor.g, userData.userChosenColor.b]);
     colors = [].concat.apply([], colors);
     var payload = [+repeatFlag, cycleSpeed].concat(colors);
@@ -43,6 +43,9 @@ function publishColors() {
     console.log("bufferPayload : ");
     console.log(bufferPayload);
 
+    if ( !client.connected ) {
+        client.reconnect();
+    }
     client.publish(topic, bufferPayload);
 }
 
@@ -68,11 +71,24 @@ app.post('/pick_color', (req, res) => {
     var data = {
         userId: userId,
         userChosenColor: JSON.parse(req.headers.color),
-        timestamp: new Date().toISOString()
+        timestamp: new Date().getTime()
     };
     console.log(data);
     updateData(userId, data);
     
+    res.sendStatus(200);
+});
+
+app.post('/adjust_params', (req, res) => {
+    cycleSpeed = +(req.headers.cycle_speed || cycleSpeed);
+    repeatFlag = !!(req.headers.repeat_flag || repeatFlag);
+    needsPublish = true;
+    res.sendStatus(200);
+});
+
+app.post('/reset', (req, res) => {
+    startTimestamp = +(req.headers.start_time_stamp || startTimestamp);
+    needsPublish = true;
     res.sendStatus(200);
 });
 
@@ -97,13 +113,5 @@ client.on('connect', () => {
 client.on('reconnect', () => {
     console.log("Connected to mqtt broker.");
 });
-
-function publishMessage(topic, message) {
-    client.publish(topic, message, {}, function(err) {
-        if(err) {
-            console.log("MQTT publish failed")
-        }
-    });
-};
 
 module.exports = app;
